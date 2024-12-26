b43a66ef5f02a01855f4c75139e12eff7418e5f4
edgar_git <48213850+EdgarMM19@users.noreply.github.com>
2024-12-26 20:10:31 +0100

    Updates

---
+++ precache-manifest.7af1d8e1015228341cf4de00d24270a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69aa229b56f67f4ee238beeba4138a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc06955d563519cacee0",
+    "url": "/static/css/main.5eaecb96.chunk.css"
+  },
+  {
+    "revision": "478bfe49854fdbf748d1",
+    "url": "/static/js/2.0a4ddba8.chunk.js"
+  },
+  {
+    "revision": "b0b8ca44cba6f5a6c307860b17dff8db",
+    "url": "/static/js/2.0a4ddba8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc06955d563519cacee0",
+    "url": "/static/js/main.92d9b199.chunk.js"
+  },
+  {
+    "revision": "2c74bec8d0737548c228",
+    "url": "/static/js/runtime-main.63bfa755.js"
+  }
+]);
